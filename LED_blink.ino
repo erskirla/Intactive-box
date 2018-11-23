6adb98e4866d6633f5120c3b13ce4f8fb487dc6a
Kenn3Th <45165531+Kenn3Th@users.noreply.github.com>
2018-11-23 10:48:43 +0100

    Update LED_blink.ino

--- LED_blink.ino
+++ LED_blink.ino
@@ -1,50 +1,3 @@
-/*************************************************** 
-  NodeMCU
-****************************************************/ 
-#include <ESP8266WiFi.h> 
-#include "Adafruit_MQTT.h" 
-#include "Adafruit_MQTT_Client.h" 
-/************************* WiFi Access Point *********************************/ 
-#define WLAN_SSID       "OsloMet MakerSpace" 
-#define WLAN_PASS       "o5lom3et2018" 
-#define MQTT_SERVER      "10.253.12.102" // static ip address
-#define MQTT_PORT         1883                    
-#define MQTT_USERNAME   "" 
-#define MQTT_PASSWORD        "" 
-#define LED_PIN         15
-#define LED_blue        12
-/************ Global State ******************/ 
-// Create an ESP8266 WiFiClient class to connect to the MQTT server. 
-WiFiClient client; 
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details. 
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD); 
-/****************************** Feeds ***************************************/ 
-// Setup a feed called 'esp8266_led' for subscribing to changes. 
-Adafruit_MQTT_Subscribe esp8266_led = Adafruit_MQTT_Subscribe(&mqtt, MQTT_USERNAME "/led/esp8266"); 
-/*************************** Sketch Code ************************************/ 
-
-void MQTT_connect(); 
-void setup() { 
- Serial.begin(115200); 
- delay(10); 
- pinMode(LED_PIN, OUTPUT); 
- digitalWrite(LED_PIN, LOW);
- // Connect to WiFi access point. 
- Serial.println(); Serial.println(); 
- Serial.print("Connecting to "); 
- Serial.println(WLAN_SSID); 
- WiFi.begin(WLAN_SSID, WLAN_PASS); 
- while (WiFi.status() != WL_CONNECTED) { 
-   delay(500); 
-   Serial.print("."); 
- } 
- Serial.println(); 
- Serial.println("WiFi connected"); 
- Serial.println("IP address: "); Serial.println(WiFi.localIP());
-  
- // Setup MQTT subscription for esp8266_led feed. 
- mqtt.subscribe(&esp8266_led); 
-}  
 uint32_t period= 5000;
 
 void loop() {
@@ -69,25 +22,3 @@ void loop() {
     } 
   } 
 }
-// Function to connect and reconnect as necessary to the MQTT server. 
-void MQTT_connect() { 
- int8_t ret; 
- // Stop if already connected. 
- if (mqtt.connected()) { 
-   return; 
- } 
- Serial.print("Connecting to MQTT... "); 
- uint8_t retries = 3; 
- while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected 
-      Serial.println(mqtt.connectErrorString(ret)); 
-      Serial.println("Retrying MQTT connection in 5 seconds..."); 
-      mqtt.disconnect(); 
-      delay(5000);  // wait 5 seconds 
-      retries--; 
-      if (retries == 0) { 
-        // basically die and wait for WDT to reset me 
-        while (1); 
-      } 
- } 
- Serial.println("MQTT Connected!"); 
-} 
